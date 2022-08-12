5f5b7a0af797d570c6c4d785437b4af5895c8241
Elias Wakile <elias.wakile@outlook.com>
2022-08-12 18:02:01 +0300

    Architecture

--- PortFolio.py
+++ PortFolio.py
@@ -1,24 +1,52 @@
 from Stock import Stock
+from StockMarket import StockData
 import pandas_datareader.data as pdr
 
 class PortFolio:
-    def __init__(self, initial_investment, stock_list):
+    def __init__(self, initial_investment, stock_list, period, interval, date_list):
+        """
+        This function is a portfolio constructor
+        :param initial_investment: The initial investment in the portfolio
+        :param stock_list: the list of stocks we wish to invest in
+        """
         self.stock_name_list = stock_list
         self.stocks = dict()
         self.balance = initial_investment
+        self.period = period
+        self.interval = interval
         for stock_name in stock_list:
-            self.stocks[stock_name] = Stock(stock_name)
+             self.stocks[stock_name] = Stock(stock_name)
+        self.date_list = date_list
 
-    def update_portfolio(self, date):
+
+    def next_day(self):
+        curr_date = self.date_list[0]
+        self.date_list = self.date_list[1:]
+        self.stock_market = {stock_name : StockData(stock_name, curr_date,
+                                                    self.period,self.interval,
+                                                    self.stocks[stock_name], 10e-1)
+                             for stock_name in self.stock_name_list}
+
+    def update_portfolio(self):
+        """
+        This function updates the portfolio for the current date
+        :param date:
+        :return:
+        """
         for stock_name in self.stock_name_list:
-            self.stocks[stock_name].update()
+            if self.stock_market[stock_name].update_stock() == 1:
+                self.next_day()
 
     def getBalance(self):
+        """
+        Gets the balance of the portfolio
+        :return:
+        """
         stock_balance = 0
-        for stock in self.stock_list:
-            stock_balance += stock.money_in_stock
+        for stock in self.stock_name_list:
+            stock_balance += self.stocks[stock].money_in_stock
         print(f"Your Portfolio currently has a balance of {self.balance}, owns"
               f"a stock of total value {stock_balance} and is of total value of"
               f"{stock_balance + self.balance}.")
-        return stock_balance + stock
+        return stock_balance + self.balance
 
--- StockMarket.py
+++ StockMarket.py
@@ -3,18 +3,34 @@ import pandas_ta as pta
 import yfinance as yf
 
 class StockData:
-    def __init__(self, name, start_date, end_date, interval, stock, momentum):
-        self.stock_data = yf.download(name, start=start_date, end=end_date, interval=interval)
+    def __init__(self, name, start_date, period, interval, stock, momentum):
+        """
+        This function initializes the stock data object
+        :param name: the stock name
+        :param start_date: the start date
+        :param end_date: the end date
+        :param interval: the time interval
+        :param stock: the stock object
+        :param momentum: the momentum of the acceleration
+        """
+        self.stock_data = yf.download(name, start=start_date, period=period, interval=interval)[29:329]
         self.stock_data["RSI"] = pta.rsi(self.stock_data["Close"], length=14)
         self.stock_data["ADX"] = pta.adx(self.stock_data["High"], self.stock_data["Low"], self.stock_data["Close"], length=7)["ADX_7"]
         self.stock_data["MACD"] = pta.macd(self.stock_data["Close"], fast=4, slow=12, signal=3)["MACDs_4_12_3"]
         self.stock_data["CCI"] = pta.cci(self.stock_data["High"], self.stock_data["Low"], self.stock_data["Close"], length=14)
         self.momentum = momentum
-        self.time_stamp = 1
+        self.time_stamp = 0
         self.stock = stock
+        self.max_time_stamp = self.stock_data.size
+        self.update_new_day()
 
     def update_stock(self):
+        """
+        This function updates the stock this object modelizes the data
+        """
         self.time_stamp += 1
+        if self.time_stamp == self.stock_data.size:
+            return 0
         stock_now = self.stock_data[self.time_stamp]
         low_price = stock_now["Low"]
         high_price = stock_now["High"]
@@ -27,3 +43,28 @@ class StockData:
         cci = stock_now["CCI"]
         self.stock.update(volume, low_price, high_price, open_price, close_price,
                      self.time_stamp, rsi, adx, cci, macd, self.momentum)
+        return 1
+
+    def update_new_day(self):
+        """
+        This function updates a stock to the new day
+        """
+        self.time_stamp += 1
+        stock_now = self.stock_data[self.time_stamp]
+        low_price = stock_now["Low"]
+        high_price = stock_now["High"]
+        open_price = stock_now["Open"]
+        close_price = stock_now["Close"]
+        volume = stock_now["Volume"]
+        rsi = stock_now["RSI"]
+        adx = stock_now["ADX"]
+        macd = stock_now["MACD"]
+        cci = stock_now["CCI"]
+        self.stock.update(volume, low_price, high_price, open_price,
+                          close_price, self.time_stamp, rsi, adx, cci, macd,
+                          self.momentum, new_day=True, momentum=10e-1)
+
+if __name__ == '__main__':
+    stock_data = yf.download("AAPL", start="2022-01-01", end="2022-01-02",
+                                  interval="1m")[29:329]
+    print(stock_data.size)
