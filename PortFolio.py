7e1e3e96726a4cc3fe5c8302d89f39006b19e7c3
Elias Wakile <elias.wakile@outlook.com>
2022-08-12 19:07:26 +0300

    Architecture

--- PortFolio.py
+++ PortFolio.py
@@ -63,27 +63,34 @@ class PortFolio:
         return stock_balance + self.balance
 
     def getState(self):
+        """
+        This function gets the current state of the Portfolio
+        :return: The state of the Portfolio, each line represents a stock and
+        the columns represent the different values of the parameters
+        """
         state = np.array([len(self.stock_name_list), self.features_num])
         for stock_name in self.stock_indices.keys():
             line_index = self.stock_indices[stock_name]
-            state[line_index, 0] = self.stocks[stock_name].self.daily_highest
-            state[line_index, 1] = self.stocks[stock_name].self.daily_lowest
-            state[line_index, 2] = self.stocks[stock_name].self.daily_precentile_acceleration
-            state[line_index, 3] = self.stocks[stock_name].self.market_volume_acceleration
-            state[line_index, 4] = self.stocks[stock_name].self.stock_price_acceleration
-            state[line_index, 5] = self.stocks[stock_name].self.per_var
-            state[line_index, 6] = self.stocks[stock_name].self.volume_var
-            state[line_index, 7] = self.stocks[stock_name].self.price_var
-            state[line_index, 8] = self.stocks[stock_name].self.current_price_daily_percentile
-            state[line_index, 9] = self.stocks[stock_name].self.last_market_volume
-            state[line_index, 10] = self.stocks[stock_name].self.last_high_price
-            state[line_index, 11] = self.stocks[stock_name].self.last_low_price
-            state[line_index, 12] = self.stocks[stock_name].self.last_open_price
-            state[line_index, 13] = self.stocks[stock_name].self.last_close_price
-            state[line_index, 14] = self.stocks[stock_name].self.ADX
-            state[line_index, 15] = self.stocks[stock_name].self.MACD
-            state[line_index, 16] = self.stocks[stock_name].self.CCI
-            state[line_index, 17] = self.stocks[stock_name].self.RSI
+            state[line_index, 0] = self.stocks[stock_name].daily_highest
+            state[line_index, 1] = self.stocks[stock_name].daily_lowest
+            state[line_index, 2] = self.stocks[stock_name].daily_precentile_acceleration
+            state[line_index, 3] = self.stocks[stock_name].market_volume_acceleration
+            state[line_index, 4] = self.stocks[stock_name].stock_price_acceleration
+            state[line_index, 5] = self.stocks[stock_name].per_var
+            state[line_index, 6] = self.stocks[stock_name].volume_var
+            state[line_index, 7] = self.stocks[stock_name].price_var
+            state[line_index, 8] = self.stocks[stock_name].current_price_daily_percentile
+            state[line_index, 9] = self.stocks[stock_name].last_market_volume
+            state[line_index, 10] = self.stocks[stock_name].last_high_price
+            state[line_index, 11] = self.stocks[stock_name].last_low_price
+            state[line_index, 12] = self.stocks[stock_name].last_open_price
+            state[line_index, 13] = self.stocks[stock_name].last_close_price
+            state[line_index, 14] = self.stocks[stock_name].ADX
+            state[line_index, 15] = self.stocks[stock_name].MACD
+            state[line_index, 16] = self.stocks[stock_name].CCI
+            state[line_index, 17] = self.stocks[stock_name].RSI
+            state[line_index, 18] = self.stocks[stock_name].num_of_stocks_owned
+            state[line_index, 19] = self.stocks[stock_name].price_per_stock
         return state
 
     def act(self, stock_predictions):
